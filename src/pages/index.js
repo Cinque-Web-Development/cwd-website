cc60d1d7da91b351b85ecabfad20eaac6ee4aff2
Samuel Trahan <samtrahan91@gmail.com>
2020-10-28 10:52:52 -0500

    message sent and getting started message

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,8 +8,7 @@ import pic01 from '../assets/images/pic01.jpg'
 import pic02 from '../assets/images/pic02.jpg'
 import pic03 from '../assets/images/pic03.jpg'
 import pic04 from '../assets/images/pic04.jpg'
-import pic05 from '../assets/images/pic05.jpg'
-import pic06 from '../assets/images/pic06.jpg'
+
 
 class HomeIndex extends React.Component {
     render() {
@@ -34,36 +33,38 @@ class HomeIndex extends React.Component {
                                 <h3>Trippin'</h3>
                                 <p>Click to start planning trips</p>
                             </header>
-                            <a href="https://trippin-cwd.herokuapp.com" className="link primary"></a>
+                            <a target="_blank" href="https://trippin-cwd.herokuapp.com" className="link primary"></a>
                         </article>
                         <article style={{backgroundImage: `url(${pic02})`}}>
                             <header className="major">
                                 <h3>Caddie</h3>
                                 <p>Click to start searching for golf courses</p>
                             </header>
-                            <a href="https://www.caddie-golf.com" className="link primary"></a>
+                            <a target="_blank" href="https://www.caddie-golf.com" className="link primary"></a>
                         </article>
                         <article style={{backgroundImage: `url(${pic03})`}}>
                             <header className="major">
                                 <h3>Collect the Cosmos</h3>
                                 <p>Click to explore the most beautiful images the universe has to offer.</p>
                             </header>
-                            <a href="https://collect-the-cosmos.herokuapp.com/" className="link primary"></a>
+                            <a target="_blank" href="https://collect-the-cosmos.herokuapp.com/" className="link primary"></a>
                         </article>
                         <article style={{backgroundImage: `url(${pic04})`}}>
                             <header className="major">
                                 <h3>PokeFaves</h3>
                                 <p>Click to collect pokemon and build your best teams.</p>
                             </header>
-                            <a href="https://pokefaves.herokuapp.com/" className="link primary"></a>
+                            <a target="_blank" href="https://pokefaves.herokuapp.com/" className="link primary"></a>
                         </article>
                     </section>
                     <section id="two">
                         <div className="inner">
                             <header className="major">
-                                <h2>Massa libero</h2>
+                                <h2>Let's build a website</h2>
                             </header>
-                            <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus pharetra. Pellentesque condimentum sem. In efficitur ligula tate urna. Maecenas laoreet massa vel lacinia pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus amet pharetra et feugiat tempus.</p>
+                            <p>Explore our different options of web applications to fit any of your website needs, from upgrading your current site to building an new application from scratch.
+            
+                            <br/> See how Cinque can build you a customized web presence. </p>
                             <ul className="actions">
                                 <li><Link to="/landing" className="button next">Get Started</Link></li>
                             </ul>
