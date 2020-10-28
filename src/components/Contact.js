c3f62d1c2dbf805c6a72e285901451ba9f862e8c
Samuel Trahan <samtrahan91@gmail.com>
2020-10-28 11:03:38 -0500

    fixed the form and the title

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,7 +7,7 @@ const Contact = (props) => {
         <section id="contact">
             <div className="inner">
                 <section>
-                    <form onSubmit={() => setEmptyState(null)} netlify>
+                    <form name="cinque" onSubmit={() => setEmptyState(null)} netlify>
                         <div className="field half first">
                             <label htmlFor="name">Name</label>
                             <input type="text" name="name" id="name" />
--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,7 @@ class HomeIndex extends React.Component {
         return (
             <Layout>
                 <Helmet
-                    title="Gatsby Starter - Forty"
+                    title="Cinque Web Development"
                     meta={[
                         { name: 'description', content: 'Sample' },
                         { name: 'keywords', content: 'sample, something' },
