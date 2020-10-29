0b5687f39cd53320a0ea0c25cb6e38d119bdf920
Samuel Trahan <samtrahan91@gmail.com>
2020-10-29 15:34:58 -0500

    finally fixed

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -25,7 +25,7 @@ const Contact = props => {
     event.preventDefault()
   }
 
-  const handleChange = e => setFormData({ [e.target.name]: e.target.value, ...formData })
+  const handleChange = e => setFormData({...formData, [e.target.name]: e.target.value})
 
   return (
     <section id="contact">
@@ -39,15 +39,15 @@ const Contact = props => {
           >
             <div className="field half first">
               <label htmlFor="name">Name</label>
-              <input onChange={handleChange} type="text" name="name" id="name" />
+              <input value={formData.name} onChange={handleChange} type="text" name="name" id="name" />
             </div>
             <div className="field half">
               <label htmlFor="email">Email</label>
-              <input onChange={handleChange} type="text" name="email" id="email" />
+              <input value={formData.email} onChange={handleChange} type="text" name="email" id="email" />
             </div>
             <div className="field">
               <label htmlFor="message">Message</label>
-              <textarea onChange={handleChange} name="message" id="message" rows="6"></textarea>
+              <textarea value={formData.message} onChange={handleChange} name="message" id="message" rows="6"></textarea>
             </div>
             <ul className="actions">
               <li>
