a124d9381518c2bc7b311e3162cc9837f019beea
kstick9210 <kstick9210@outlook.com>
2021-02-19 14:07:17 -0600

    make form fields required

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -39,15 +39,15 @@ const Contact = props => {
           >
             <div className="field half first">
               <label htmlFor="name">Name</label>
-              <input value={formData.name} onChange={handleChange} type="text" name="name" id="name" />
+              <input value={formData.name} onChange={handleChange} type="text" name="name" id="name" required/>
             </div>
             <div className="field half">
               <label htmlFor="email">Email</label>
-              <input value={formData.email} onChange={handleChange} type="text" name="email" id="email" />
+              <input value={formData.email} onChange={handleChange} type="text" name="email" id="email" required/>
             </div>
             <div className="field">
               <label htmlFor="message">Message</label>
-              <textarea value={formData.message} onChange={handleChange} name="message" id="message" rows="6"></textarea>
+              <textarea value={formData.message} onChange={handleChange} name="message" id="message" rows="6" required></textarea>
             </div>
             <ul className="actions">
               <li>
