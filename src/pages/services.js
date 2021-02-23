bf1981c01c9ecfa9bb0533bac89e59f2dff0acdc
kstick9210 <kstick9210@outlook.com>
2021-02-23 15:00:18 -0600

    update services

--- src/pages/services.js
+++ src/pages/services.js
@@ -7,6 +7,7 @@ import BannerLanding from '../components/BannerLanding'
 import pic08 from '../assets/images/pic08.jpg'
 import pic09 from '../assets/images/pic09.jpg'
 import pic10 from '../assets/images/pic10.jpg'
+import pic11 from '../assets/images/pic11.jpg'
 
 const Services = (props) => (
     <Layout>
@@ -23,52 +24,66 @@ const Services = (props) => (
                     <header className="major">
                         <h2>About Cinque</h2>
                     </header>
-                    <p>Cinque Web Development consists of a team of experienced developers who have a wide range of knowledge, building websites, passion projects, and mobile applications.</p>
+                    <p>Cinque Web Development consists of a team of experienced developers who have a wide range of knowledge, building websites and passion projects.</p>
                 </div>
             </section>
             <section id="two" className="spotlights">
                 <section>
-                    <Link to="/websites" className="image">
+                    <Link to="/" className="image">
                         <img src={pic08} alt="" />
                     </Link>
                     <div className="content">
                         <div className="inner">
                             <header className="major">
-                                <h3>Websites</h3>
+                                <h3>Business Websites</h3>
                             </header>
-                            <p>Our team of experienced developers can build you simple and scalable websites for your business, or help you implement a new feature on your existing site.</p>
+                            <p>Our team can build you a polished and scalable website for your business, or help you implement a new feature on your existing site.</p>
                             <ul className="actions">
-                                <li><Link to="/websites" className="button">Learn more</Link></li>
+                                <li><Link to="/" className="button">See Examples</Link></li>
                             </ul>
                         </div>
                     </div>
                 </section>
                 <section>
-                    <Link to="/mobileapps" className="image">
+                    <Link to="/" className="image">
                         <img src={pic09} alt="" />
                     </Link>
                     <div className="content">
                         <div className="inner">
                             <header className="major">
-                                <h3>Mobile Applications</h3>
+                                <h3>Personal Websites</h3>
                             </header>
-                            <p>Have an idea for a mobile app? Our team can help that dream become a reality.</p>
+                            <p>Cinque's developers will work with you to design a website to suit your unique needs, whether you are showcasing your photography, publishing your blog, or reviewing your new favorite movie.</p>
                             <ul className="actions">
-                                <li><Link to="/mobileapps" className="button">Learn more</Link></li>
+                                <li><Link to="/" className="button">See Live Demo</Link></li>
                             </ul>
                         </div>
                     </div>
                 </section>
                 <section>
-                        <img src={pic10} alt="" className="image"/>
+                    <Link to="/" className="image">
+                        <img src={pic10} alt="" />
+                    </Link>
                     <div className="content">
                         <div className="inner">
                             <header className="major">
-                                <h3>CSS Framework</h3>
+                                <h3>Marketing Websites</h3>
                             </header>
-                            <p>Coming Soon</p>
+                            <p>If you are preparing to launch a new product, our team can build you a customized landing page that will take your marketing to the next level.</p>
                             <ul className="actions">
+                                <li><Link to="/" className="button">See Live Demo</Link></li>
                             </ul>
+                        </div>
+                    </div>
+                </section>
+                <section>
+                    <img src={pic11} alt="" className="image"/>
+                    <div className="content">
+                        <div className="inner">
+                            <header className="major">
+                                <h3>Contact us if these options don't fit</h3>
+                            </header>
+                            <p>Something something customizable solution</p>
                         </div>
                     </div>
                 </section>
